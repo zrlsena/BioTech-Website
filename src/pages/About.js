8d0e31a2f416fd9633a9f2596c46713582179ee9
Sena Zorlu <senazorlux@gmail.com>
2024-05-22 19:43:37 +0300

    team page

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
   useEffect(() => {
     ScrollReveal({ reset: true, distance: '60px', duration: 2500, delay: 400 });
 
-    ScrollReveal().reveal('.main-title, .section-title', { delay: 500, origin: 'left' });
+    ScrollReveal().reveal('.main-title', { delay: 500, origin: 'left' });
     ScrollReveal().reveal('.sec-01 .image, .info', { delay: 600, origin: 'bottom' });
     ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' });
     ScrollReveal().reveal('.sec-02 .image', { delay: 500, origin: 'top' });
@@ -44,7 +44,6 @@ function About() {
       </section>
       <section className="sec-02">
         <div className="container">
-          <h3 className="section-title">Vision</h3>
           <div className="content">
             <div className="image">
               <img src="/images/5.jpg" alt=""/>
@@ -52,10 +51,9 @@ function About() {
             <div className="info">
               <h4 className="info-title">Our Vision</h4>
               <ul>
-                <li>To play a catalytic role in transforming the education received from academia in the field of rare diseases and nanotechnology into products for industry</li>
-                <li>Supporting young scientists and supporting their projects and development.</li>
-                <li>Increasing social awareness in rare diseases.</li>
-                <li>To facilitate nanoparticle production, enabling accessible and affordable drug production.</li>
+                <li>Reducing antibiotic resistance by treating various diseases, especially biofilm infections with nanoparticles.</li>
+                <li>Using nanoparticles to facilitate applications in agriculture and other sectors making nanoparticle production efficient and easy.</li>
+                <li>To protect nature and raise awareness of rare diseases while conducting our activities as a startup.</li>
               </ul>
 
             </div>
