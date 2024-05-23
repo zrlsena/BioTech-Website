697fe565d592a35a91dc23fc55ee4c385d6f34b4
Sena Zorlu <senazorlux@gmail.com>
2024-05-24 02:49:42 +0300

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,34 +2,38 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 function Home() {
-  return (<div>
-    <Navbar/>
+  return (
+
+
     <div>
-    <section>
-      
-      <div className="home">
-        <h1 className="main-title">Next Generation <br/> NanoBioTech Solutions</h1>
-<div>
-          <img src="./images/microscope.png" alt=""/>
-          </div>
-    <div>
-      
-        
+      <Navbar />
+      <div>
+        <section>
+  
+          <div className="home">
+            <h1 className="main-title">Next Generation <br /> NanoBioTech Solutions</h1>
             <div>
-              <h3>NanoWISS</h3>
-              <ul>
-                <li>nanoWISS is a deep technology and social impact startup that aims to faciliate applications in areas such as 
-                  treatment and agriculture that produce nanoparticles that set out with the ideal of treating biofilm bacterial 
-                  infections in cystic fibrosis disease and allowing patients to breathe deeper and aims to make nanoparticle production 
-                  efficient and to raise awareness of rare diseases.</li>
-                
-              </ul>
+              <img src="./images/microscope.png" alt="" />
+            </div>
+            <div>
+  
+  
+              <div className="home fcontent">
+                <h3>NanoWISS</h3>
+                <ul>
+                  <li>nanoWISS is a deep technology and social impact startup that aims to faciliate applications in areas such as <br />
+                    treatment and agriculture that produce nanoparticles that set out with the ideal of treating<br /> biofilm bacterial
+                    infections in cystic fibrosis disease and allowing patients <br /> to breathe deeper and aims to make nanoparticle production
+                    <br /> efficient and to raise awareness of rare diseases.</li>
+  
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
-    </div>
+    
   );
 }
 
