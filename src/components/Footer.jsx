d852054b7ace78e4f827bf4f3f46202e64e2f481
Sena Zorlu <senazorlux@gmail.com>
2024-05-24 05:49:16 +0300

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
     <footer>
       <p className="btn"> © {currentYear} Sena Zorlu. All rights reserved.</p>
       
-      <a  href="https://www.instagram.com/nanowiss.co?igsh=c2wzaHN1azU5Nm80"><box-icon name='instagram' type='logo' color='#0071e3' ></box-icon></a>
-      <a  href="info@nanowiss.com"><box-icon type='solid' name='envelope' color='#0071e3'></box-icon></a>
+      <a  href="https://www.instagram.com/nanowiss.co?igsh=c2wzaHN1azU5Nm80"><box-icon name='instagram' type='logo' color='#86868B' ></box-icon></a>
+      <a  href="mailto:info@nanowiss.com"><box-icon type='solid' name='envelope' color='#86868B'></box-icon></a>
 
     </footer>
   );
