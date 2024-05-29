44489da16f5f21b69c95c06226d96a39c941102d
Sena Zorlu <senazorlux@gmail.com>
2024-05-29 19:16:10 +0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
   useEffect(() => {
     ScrollReveal({ reset: true, distance: '60px', duration: 2500, delay: 400 });
     ScrollReveal().reveal('.home .main-title', { delay: 500, origin: 'left' });
-    ScrollReveal().reveal('.home .fcontent', { delay: 700, origin: 'right' });
+    ScrollReveal().reveal('.home .fcontent2 .image2 img', { delay: 300, origin: 'right' });
 
   }, []);
 
@@ -37,6 +37,25 @@ function Home() {
 
                   </ul>
                 </div>
+
+                <div className="home fcontent2">
+                <div className="pa">
+                      <h3 className="pa-title">Projects</h3>
+                      <ul>
+                        <li>Reducing antibiotic resistance by treating various diseases, especially biofilm infections with nanoparticles.</li>
+                        <li>Using nanoparticles to facilitate applications in agriculture and other sectors making nanoparticle production efficient and easy.</li>
+                        <li>To protect nature and raise awareness of rare diseases while conducting our activities as a startup.</li>
+                      </ul>
+                  </div>
+                    <div className="image2">
+                      <img src="/images/5.jpg" alt="" />
+                      <img src="/images/4.jpg" alt="" /><br/>
+                      <img src="/images/2.jpg" alt="" />
+                      <img src="/images/3.jpg" alt="" />
+                    </div>
+                    
+                </div>
+
               </div>
             </div>
           </section>
